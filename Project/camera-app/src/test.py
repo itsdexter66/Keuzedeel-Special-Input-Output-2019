69cc8125715646dcc171f8ce5388ea2f356a03fa
AlexJanson <alex@hoofddorp.nl>
2019-06-07 09:17:03 +0200

    Special keys like arrow keys and shift are now supported.

--- Project/camera-app/src/test.py
+++ Project/camera-app/src/test.py
@@ -3,6 +3,25 @@
 import sys
 from pynput.keyboard import Key, Controller
 
+def switch(arg):
+    switcher = {
+        'down': Key.down,
+        'up': Key.up,
+        'left': Key.left,
+        'right': Key.right,
+        'ctrl': Key.ctrl,
+        'shift': Key.shift,
+        'space': Key.space,
+        'tab': Key.tab,
+        'alt': Key.alt,
+        'enter': Key.enter,
+        'delete': Key.delete,
+        'esc': Key.esc,
+        'backspace': Key.backspace
+    }
+    return switcher.get(arg, arg)
+
 keyboard = Controller()
-keyboard.press(sys.argv[1])
-keyboard.release(sys.argv[1])
+key = switch(sys.argv[1])
+keyboard.press(key)
+keyboard.release(key)
